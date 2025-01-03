534ea999f1a52893961e4cc5fc44eda78afbc813
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-01-03 17:37:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+@app.route("/")
+def landingpage():
+    return render_template('index.html')
 @app.route("/signin", methods=['GET', 'POST'])
 def login():
     return render_template('signin.html')
@@ -8,6 +11,8 @@ def login():
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
     return render_template('signup.html')  
-
+@app.route("/privacy")
+def ldngpg():
+    return render_template('privacy-policy.html')
 if __name__ == "__main__":
     app.run(debug=True)
