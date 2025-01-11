c4698b7e9a66d366d48fa012da9edd1dea3f964b
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-01-11 16:56:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ app = Flask(__name__)
 @app.route("/")
 def landingpage():
     return render_template('index.html')
-@app.route("/signin", methods=['GET', 'POST'])
+@app.route("/auth/signin", methods=['GET', 'POST'])
 def login():
     return render_template('signin.html')
 
-@app.route("/signup", methods=['GET', 'POST'])
+@app.route("/auth/signup", methods=['GET', 'POST'])
 def signup():
     return render_template('signup.html')  
-@app.route("/privacy")
+@app.route("/help/privacy")
 def privacy():
     return render_template('privacy-policy.html')
-@app.route("/feedback")
+@app.route("/help/feedback")
 def feedback():
     return render_template('feedback.html')
 if __name__ == "__main__":
